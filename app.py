96acb800ad913462fd257e749762f4c20e9cce2a
Desaboina Krishna Sai <42516247+KrrishDDVKS@users.noreply.github.com>
2025-03-13 23:11:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -67,6 +67,7 @@ def reg(x):
                 st.warning("Please enter both userid and password.")
 
 def display_pdf(pdf_bytes):
+    st.warning('Please don't upload PDF file. The PDF file viwer is not working on Cloud.')
     base64_pdf = base64.b64encode(pdf_bytes).decode('utf-8')
     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="600" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
