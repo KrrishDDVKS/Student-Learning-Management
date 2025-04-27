f381336d22956a96fd3f06f7a0aac617cc2fe5c2
Desaboina Krishna Sai <42516247+KrrishDDVKS@users.noreply.github.com>
2025-04-27 17:29:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -37,27 +37,27 @@ ansd=db["Ansd"]
 fs = gridfs.GridFS(db)
 fsa = gridfs.GridFS(dba)
 # Session state initialization
-if "reg_in" not in st.session_state:
+if "reg_in" in st.session_state:
     st.session_state["reg_in"] = False
-if "logged_in" not in st.session_state:
+if "logged_in"  in st.session_state:
     st.session_state["logged_in"] = False
     st.session_state["userid"] = ""
     st.session_state["role"] = ""
-if "messages" not in st.session_state:
+if "messages"  in st.session_state:
     st.session_state.messages = {}  # Store chat history per user
-if "admin_joined" not in st.session_state:
+if "admin_joined"  in st.session_state:
     st.session_state.admin_joined = {}
-if 'mar' not in st.session_state:
+if 'mar'  in st.session_state:
     st.session_state.mar=0
-if 'mark' not in st.session_state:
+if 'mark'  in st.session_state:
     st.session_state.mark=0
-if 'exam' not in st.session_state:
+if 'exam'  in st.session_state:
     st.session_state.exam=0
-if 'descriptive' not in st.session_state:
+if 'descriptive'  in st.session_state:
     st.session_state.descriptive=0
-if 'call' not in st.session_state:
+if 'call'  in st.session_state:
     st.session_state.call=0
-if "ms" not in st.session_state:
+if "ms"  in st.session_state:
     st.session_state["ms"] = [{"role": "assistant", "content": "Enter your profession to get course recommendations:"}]
 
 #Logout Function
@@ -616,7 +616,7 @@ def maini():
             for i in x:
                 st.write(f'{i["question"]}')
                 st.write(f'Ans: {i["ans"]}')
-                dmar=int(st.text_input(f'marks out of {i['tmark']}'),0)
+                dmar=int(st.text_input(f'marks out of {i["tmark"]}'),0)
                 stu=i['id']
             if st.button('Grade'):
                 st.session_state.descriptive=st.session_state.descriptive+1
